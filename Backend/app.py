c9fc381534c4854e1ccccbafe2714fe6e9c14c67
prithvi1236 <bhargav.prithvi2005@gmail.com>
2024-10-07 19:00:56 +0530

    Connection solved

--- Backend/app.py
+++ Backend/app.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 
 from flask import Flask, request, jsonify, render_template, send_file
 import pandas as pd
+from flask_cors import CORS
 from io import BytesIO
 app = Flask(__name__)
-
+CORS(app)
 # Load the dataset
 file_path = 'dataset_nasa.xlsx'
 dfs = pd.read_excel(file_path, sheet_name=None)
@@ -20,13 +21,14 @@ df3 = dfs['SO2 Population-Weighted (ppm)']
 def home():
     return render_template('index.html')
 
-@app.route('/country', methods=['POST'])
+@app.route('/country', methods=['GET'])
 def country():
-    country = request.form.get('country').lower()  # Get country from form, convert to lowercase
-    matched_countries = df1['country'].str.lower().str.startswith(country)  # Match with lowercase countries
-    result = df1[matched_countries]['country'].unique()  # Get unique matched countries
-    # list(result)[:6]
-    return jsonify(["India"])  # Return maximum 6 matches as JSON
+    try:
+        country = request.args.get('country')
+        if country not in df1['country'].values:
+            return jsonify({"error": "Country not found in the dataset"}), 404
+    except:
+        return jsonify({"error": "Invalid request"}), 400  # Return maximum 6 matches as JSON
 
 
 @app.route('/co2_plot', methods=['POST'])
